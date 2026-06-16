33bdb317dd66bc868e2555264e68a466c15d3a4c
Coding Hub <codinghub300@gmail.com>
2026-06-16 06:38:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ url = "https://raw.githubusercontent.com/TrainingByPackt/Interactive-Data-Visual
 
 df = pd.read_csv(url)
 
-st.write(df.tail(10))
+st.dataframe(df, use_container_width=True, height=500)
 
 internet_2016 = df[df["Year"] == 2016]
 
