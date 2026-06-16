7336ab9dc7ef2bf779530d42ef227313ed062e2d
Coding Hub <codinghub300@gmail.com>
2026-06-16 06:12:02 +0300

    Display top 10 rows of DataFrame instead of 5

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ url = "https://raw.githubusercontent.com/TrainingByPackt/Interactive-Data-Visual
 df = pd.read_csv(url)
 
 # st.write(df.columns.tolist())
-st.write(df.head())
+st.write(df.head(10))
 
 internet_2016 = df[df["Year"] == 2016]
 
